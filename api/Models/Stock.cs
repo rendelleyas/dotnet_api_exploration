adacbb7b6d82604bc6ca6367601fd235cde6afdd
Rendell Eyas <reyas@netlinkvoice.com>
2024-08-06 11:14:17 +0800

    up: db migration and ef setup

--- api/Models/Stock.cs
+++ api/Models/Stock.cs
@@ -20,5 +20,7 @@ namespace api.Models
         public string Industry { get; set; } = string.Empty;
 
         public long MarketCap { get; set; }
+
+        public List<Comment> Comments { get; set; } = new List<Comment>();
     }
 }
